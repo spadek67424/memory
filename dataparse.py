843190b2631567dbc0f31f38928ed9f1063fcba4
minghwu <spadek67424@gmail.com>
2024-03-25 22:04:07 -0400

    add combined algorithm.

--- dataparse.py
+++ dataparse.py
@@ -67,7 +67,87 @@ def greedy_algorithm(func, process_list):
         overlaploss = backuplist
     functionstring.append(nextindex)
     print(functionstring)
-    
+
+
+def greedy_combined_algorithm(func, process_list):
+    overlaploss = list()
+    function_set =dict()
+    for i in process_list:
+        if process_list[i][0] in function_set.keys():
+            if i not in function_set[process_list[i][0]]:
+                function_set[process_list[i][0]].append(i)
+        else:
+            function_set[process_list[i][0]] = list()
+            function_set[process_list[i][0]].append(i)
+    for i in function_set.keys():
+        print(i)
+        print(function_set[i])
+    for i in function_set:
+        for  j in function_set:
+            if i == j:
+                continue
+            notUnionProcess = list()
+            array1=list()
+            array2=list()
+            for k in i.split(", "):
+                array1.append(k)
+            for k in j.split(", "):
+                array2.append(k)
+            for k in array1:
+                if k not in array2:
+                    notUnionProcess.append(k)
+            for k in array2:
+                if k not in array1:
+                    notUnionProcess.append(k)
+            overlaploss.append((function_set[i],function_set[j], len(notUnionProcess)))
+    print(len(overlaploss))
+    minloss = np.iinfo(np.int32).max
+    target = 0
+    ## find the minimum
+    global TotalLossgreedy
+    TotalLossgreedy = 0
+    functionstring = list()
+    #### First round of greedy algorithm.
+    minloss = np.iinfo(np.int32).max
+    for i in overlaploss:
+        if(i[2] < minloss):
+            target = i
+            minloss = i[2]
+    TotalLossgreedy +=minloss
+    removeindex = target[0]
+    nextindex = target[1]
+    functionstring.append(removeindex)
+    backuplist = list()
+    for i in overlaploss:
+        if i[0] == removeindex or i[1] == removeindex:
+            continue
+        backuplist.append(i)
+    overlaploss = backuplist
+    #######
+    while(len(overlaploss) > 0):  
+        backuplist = list()  
+        for i in overlaploss:
+            if i[0]== nextindex:
+                backuplist.append(i)
+        minloss = np.iinfo(np.int32).max
+        for i in backuplist:
+            if(i[2] < minloss):
+                target = i
+                minloss = i[2]
+        TotalLossgreedy +=minloss
+        removeindex = target[0]
+        nextindex = target[1]
+        functionstring.append(removeindex)
+        backuplist = list() 
+        for i in overlaploss:
+            if i[0] == removeindex or i[1] == removeindex:
+                continue
+            backuplist.append(i)
+        overlaploss = backuplist
+    functionstring.append(nextindex)
+    print(functionstring)
+
+ 
 def readsize():
     with open('./data/symbol_size.txt', 'r') as days_file:
         size_dict = dict()
@@ -94,7 +174,8 @@ def readfile():
 if __name__ == "__main__":   
     func = readsize() 
     process_list = readfile()
-    greedy_algorithm(func, process_list)
+    #greedy_algorithm(func, process_list)
+    greedy_combined_algorithm(func, process_list)
     print(TotalLossgreedy)
     
     
